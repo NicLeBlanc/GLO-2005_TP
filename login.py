e663305fd9bbf52bad564e42f13de21fdcf02c78
nic1279 <80590945+nic1279@users.noreply.github.com>
2021-03-23 20:41:16 -0400

    bug, pas capable de display dans le bienvenu ...

--- login.py
+++ login.py
@@ -43,16 +43,25 @@ def login():
     return render_template('login.html', message="Les informations entrées ne sont pas valides, veuillez ré-essayer")
 
 
+@app.route("/bienvenu", methods=['POST', 'GET'])
 def recent_books():
-    top10_books = recent_books()
 
-    response = {
-        "status": 200,
-        "top10_books": top10_books
-    }
+    conn = pymysql.connect(
+        host="127.0.0.1",
+        user="root123",
+        password="123",
+        db="livres_en_vrac")
 
-    return jsonify(response)
-    
+    cmd = 'SELECT * FROM Livres ORDER BY annee_publication DESC limit 0,1;'
+    cur = conn.cursor()
+    cur.execute(cmd)
+    info = cur.fetchone()
+
+    global livres
+    livres["titre"] = info[2]
+    # ProfileUtilisateur["prenom"] = info[2]
+    # ProfileUtilisateur["nom"] = info[3]
+    return render_template('bienvenu.html', livres=livres)
 
 if __name__ == "__main__":
     app.run()
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from faker import Faker
-from database import insert_into_clients
+import pymysql.cursors
 
 fake_data = Faker()
 
@@ -9,3 +9,15 @@ liste_vide = []
 for x in range(len(list)):
     liste_vide.append([item[x] for item in list])
 print(liste_vide)
+
+conn = pymysql.connect(
+        host="127.0.0.1",
+        user="root123",
+        password="123",
+        db="livres_en_vrac")
+
+cmd = 'SELECT * FROM Livres ORDER BY annee_publication DESC limit 0,10;'
+cur = conn.cursor()
+cur.execute(cmd)
+top10_books = cur.fetchall()
+print(top10_books)
