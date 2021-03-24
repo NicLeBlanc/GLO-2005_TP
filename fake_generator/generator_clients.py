c922c9d5f93fccfa8b9bc4a668493ba1d5aafc7e
Francois Lemieux-Tremblay <francois.lemieux-tremblay.1@ul;aval.ca>
2021-03-24 16:31:04 -0400

    fake password implanté, reste a eliminer les doublons de isbn

--- fake_generator/generator_clients.py
+++ fake_generator/generator_clients.py
@@ -21,11 +21,13 @@ for full_name in fake_full_name:
 fake_email = []
 for i in range(1000):
     fake_email.append(fake_data.email())
+#print(fake_email)
+#print("\n")
 
 # List of fake password
-fake_password = []
-for i in range(1000):
-    fake_password.append(fake_data.password())
+#fake_password = []
+#for i in range(1000):
+#    fake_password.append(fake_data.password())
 
 # List of fake date of birth, between 18 yo and 100 yo
 fake_birthdate = []
@@ -40,7 +42,7 @@ for i in range(1000):
 
 # List of fake profiles
 fake_profiles = []
-fake_list = [fake_email, fake_password, fake_first_name, fake_last_name, fake_adress, fake_birthdate]
+fake_list = [fake_email, fake_first_name, fake_last_name, fake_adress, fake_birthdate]
 for x in range(len(fake_email)):
     fake_profiles.append([item[x] for item in fake_list])
 print(fake_profiles)
