1139a4889993cc36342730415df96d6d7b49b0bd
nic1279 <80590945+nic1279@users.noreply.github.com>
2021-03-24 23:50:43 -0400

    modifs

--- login.py
+++ login.py
@@ -42,26 +42,26 @@ def login():
     return render_template('login.html', message="Les informations entrées ne sont pas valides, veuillez ré-essayer")
 
 
-# @app.route("/bienvenu", methods=['POST', 'GET'])
-# def recent_books():
-#
-#     conn = pymysql.connect(
-#         host="127.0.0.1",
-#         user="root123",
-#         password="123",
-#         db="livres_en_vrac")
-#
-#     cmd = 'SELECT * FROM Livres ORDER BY annee_publication DESC limit 0,2;'
-#     cur = conn.cursor()
-#     cur.execute(cmd)
-#     info = cur.fetchone()
-#
-#     global livres
-#     livres["titre"] = info[2]
-#     livres["auteur"] = info[3]
-#     # ProfileUtilisateur["nom"] = info[3]
-#
-#     return render_template('bienvenu.html', livres=livres)
+@app.route("/bienvenu", methods=['POST', 'GET'])
+def recent_books():
+
+    conn = pymysql.connect(
+        host="127.0.0.1",
+        user="root123",
+        password="123",
+        db="livres_en_vrac")
+
+    cmd = 'SELECT * FROM Livres ORDER BY annee_publication DESC limit 0,2;'
+    cur = conn.cursor()
+    cur.execute(cmd)
+    info = cur.fetchone()
+
+    global livres
+    livres["titre"] = info[2]
+    livres["auteur"] = info[3]
+    # ProfileUtilisateur["nom"] = info[3]
+
+    return render_template('bienvenu.html', livres=livres)
 
 if __name__ == "__main__":
     app.run()
